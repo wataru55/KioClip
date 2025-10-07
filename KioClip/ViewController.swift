341f3799daa6a5ff228d7d7f5881e022e60ca284
wataru55 <wataru200155@gmail.com>
2025-10-07 15:24:38 +0900

    Initial Commit

---
+++ KioClip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KioClip
+//
+//  Created by 高橋和 on 2025/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KioClipTests/KioClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  KioClipTests.swift
+//  KioClipTests
+//
+//  Created by 高橋和 on 2025/10/07.
+//
+
+import Testing
+@testable import KioClip
+
+struct KioClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
