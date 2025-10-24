aff492ce78f4492e7baf405509b6fe913fcac487
wataru55 <wataru200155@gmail.com>
2025-10-24 16:34:58 +0900

    feature: URLの一覧を表示する画面を作成

---
+++ KioClip/ArticleListViewController.swift
@@ -0,0 +1,121 @@
+import UIKit
+
+class ArticleListViewController: UIViewController {
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            ArticleTableViewCell.self, forCellReuseIdentifier: "ArticleTableViewCell")
+        return tableView
+    }()
+
+    private let addButton = AddButton()
+
+    private let listTitle: String
+    private var articles: [Article] = []
+
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    // 統合された初期化
+    init(title: String, articles: [Article] = []) {
+        self.listTitle = title
+        self.articles = articles
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        title = listTitle
+
+        setupSearchController()
+        setupUI()
+        setupDataSource()
+    }
+
+    private func setupSearchController() {
+        searchController.searchBar.placeholder = "検索"
+
+        navigationItem.searchController = searchController
+
+        // 5. 常に表示したければ false にする (オプション)
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        view.addSubview(addButton)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+
+            addButton.trailingAnchor.constraint(
+                lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            addButton.bottomAnchor.constraint(
+                lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            addButton.widthAnchor.constraint(equalToConstant: 50),
+            addButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+
+    private func setupDataSource() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    @objc private func addButtonTapped() {
+        let modalVC = ModalViewController(type: ModalViewControllerType.article)
+        let navVC = UINavigationController(rootViewController: modalVC)
+
+        if let sheet = navVC.sheetPresentationController {
+            let smallDetent = UISheetPresentationController.Detent.custom(
+                identifier: .init("small")
+            ) { context in
+                // ここで好きな高さを返す
+                return 200
+            }
+            sheet.detents = [smallDetent]
+            sheet.largestUndimmedDetentIdentifier = nil
+            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+        }
+
+        present(navVC, animated: true, completion: nil)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ArticleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath)
+            as! ArticleTableViewCell
+        cell.configure(with: articles[indexPath.row])
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+// Cellがタップされたときの処理を加えるための拡張
+extension ArticleListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let article = articles[indexPath.row]
+        if let url = URL(string: article.url) {
+            UIApplication.shared.open(url)
+        }
+    }
+}
---
+++ KioClip/ArticleTableViewCell.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.numberOfLines = 2
+        return label
+    }()
+
+    private let urlLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 1
+        return label
+    }()
+
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12, weight: .regular)
+        label.textColor = .tertiaryLabel
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(urlLabel)
+        contentView.addSubview(dateLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(
+                equalTo: contentView.trailingAnchor, constant: -16),
+
+            urlLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            urlLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            urlLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            dateLabel.topAnchor.constraint(equalTo: urlLabel.bottomAnchor, constant: 4),
+            dateLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            dateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
+        ])
+    }
+
+    func configure(with article: Article) {
+        titleLabel.text = article.title
+        urlLabel.text = article.url
+
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        dateLabel.text = "保存日: \(formatter.string(from: article.savedDate))"
+    }
+}
