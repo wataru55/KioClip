b2c9bb1518f625f6a5996a91b2d7d0c2db89e49d
wataru55 <wataru200155@gmail.com>
2025-11-10 18:08:24 +0900

    fix: 軽微な修正

--- KioClip/Model/Article.swift
+++ KioClip/Model/Article.swift
@@ -5,18 +5,18 @@
 //  Created by 高橋和 on 2025/10/10.
 //
 
-import UIKit
 import SwiftData
+import UIKit
 
 @Model
 final class Article {
-    public private(set) var id: String
-    public private(set) var createdAt: Date
-    public private(set) var url: String
-    
+    private(set) var id: String
+    private(set) var createdAt: Date
+    private(set) var url: String
+
     @Relationship(deleteRule: .nullify)
     var groups: [Group] = []
-    
+
     init(url: String, createdAt: Date = Date()) {
         self.id = UUID().uuidString
         self.createdAt = createdAt
